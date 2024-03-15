d580aef2009a01df3fc25277907394b051e28fc2
tanghongxin <lengsukq@foxmail.com>
2024-03-15 09:19:58 +0800

    异步问题优化解决

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const fileEncode = process.argv[4]
 const fileExt = process.argv[5]
 const user = process.argv[6]
 const filePath = path.join(fileDir, fileName)
-const dateFun = require('C:\\Users\\Administrator\\Downloads\\file-header-main\\throttle.js')
+const {throttle} = require('C:\\Users\\Administrator\\Documents\\code\\file-header\\throttle.js')
 const isLess = num => num < 10 ? ('0' + num) : num
 
 const timeFormat = now => {
@@ -18,49 +18,52 @@ const timeFormat = now => {
 console.log(`path: ${filePath}, encode: ${fileEncode}, ext: ${fileExt}`)
 
 try {
-  
-  const mainR = dateFun.main();
-  if (!mainR){
-    console.log('未通过',mainR)
-    return
-  }else{
-    console.log('通过',mainR)
-  }
-  const source = fs.readFileSync(filePath, {
-    encoding: fileEncode
-  })
-  let sourceStr = source.toString()
 
-  const editor =
-    sourceStr.match(/(@LastEditors: )(.*)/) ? sourceStr.match(/(@LastEditors: )(.*)/)[0] : false
+  throttle().then(mainR => {
+    if (!mainR){
+      console.log('app-未通过',mainR)
+      return
+    }else{
+      console.log('app-通过',mainR)
+      const source = fs.readFileSync(filePath, {
+        encoding: fileEncode
+      })
+      let sourceStr = source.toString()
 
-  const time =
-    sourceStr.match(/(@LastEditTime: )(.*)/) ? sourceStr.match(/(@LastEditTime: )(.*)/)[0] : false
+      const editor =
+          sourceStr.match(/(@LastEditors: )(.*)/) ? sourceStr.match(/(@LastEditors: )(.*)/)[0] : false
 
-  const date = timeFormat(new Date())
+      const time =
+          sourceStr.match(/(@LastEditTime: )(.*)/) ? sourceStr.match(/(@LastEditTime: )(.*)/)[0] : false
 
-  if (!editor || !time) {
+      const date = timeFormat(new Date())
 
-    let header;
-    if (['js', 'ts','jsx', 'tsx'].includes(fileExt)) {
-      header = `/*\n * @Author: ${user}\n * @Date: ${date}\n * @LastEditors: ${user}\n * @LastEditTime: ${date}\n * @Description: desc\n */\n`;
-    } else if (['vue', 'html'].includes(fileExt)) {
-      header = `<!--\n * @Author: ${user}\n * @Date: ${date}\n * @LastEditors: ${user}\n * @LastEditTime: ${date}\n * @Description: desc\n -->\n`;
-    } else {
-      // For other file types, you can define a default header here.
-      header = `/*\n * @Author: ${user}\n * @Date: ${date}\n * @LastEditors: ${user}\n * @LastEditTime: ${date}\n * @Description: desc\n */\n`;
+      if (!editor || !time) {
+
+        let header;
+        if (['js', 'ts','jsx', 'tsx'].includes(fileExt)) {
+          header = `/*\n * @Author: ${user}\n * @Date: ${date}\n * @LastEditors: ${user}\n * @LastEditTime: ${date}\n * @Description: desc\n */\n`;
+        } else if (['vue', 'html'].includes(fileExt)) {
+          header = `<!--\n * @Author: ${user}\n * @Date: ${date}\n * @LastEditors: ${user}\n * @LastEditTime: ${date}\n * @Description: desc\n -->\n`;
+        } else {
+          // For other file types, you can define a default header here.
+          header = `/*\n * @Author: ${user}\n * @Date: ${date}\n * @LastEditors: ${user}\n * @LastEditTime: ${date}\n * @Description: desc\n */\n`;
+        }
+
+        sourceStr = header + sourceStr
+
+      } else {
+
+        sourceStr = sourceStr.replace(editor, `@LastEditors: ${user}`)
+        sourceStr = sourceStr.replace(time, `@LastEditTime: ${date}`)
+
+      }
+
+      fs.writeFileSync(filePath, sourceStr, { encoding: fileEncode })
     }
+  })
 
-    sourceStr = header + sourceStr
 
-  } else {
-
-    sourceStr = sourceStr.replace(editor, `@LastEditors: ${user}`)
-    sourceStr = sourceStr.replace(time, `@LastEditTime: ${date}`)
-
-  }
-
-  fs.writeFileSync(filePath, sourceStr, { encoding: fileEncode })
 
 } catch (e) {
   console.log(e)
--- throttle.js
+++ throttle.js
@@ -1,55 +1,54 @@
 const fs = require('fs');
+const fs2 = require('fs').promises;
 const path = require('path');
 const currentDate = new Date().toISOString();
 
 const filePath = path.join(__dirname, 'date.json');
-function getDate() {
-  fs.readFile(filePath, 'utf-8', (readErr, data) => {
-    if (readErr) {
-      console.error('Error reading file:', readErr);
-    } else {
-      try {
-        const jsonData = JSON.parse(data);
-        console.log('Time from file:', jsonData.date);
-        const oldTime = jsonData.date;
-        const diff = Math.abs((new Date(currentDate) - new Date(oldTime)));
-        if (diff >= 5000){
-          console.log('通过',diff);
-          setDate();
-          return true;
-        }else{
-          console.log('不通过',diff)
 
-          return false;
-        }
-      } catch (parseErr) {
-        console.error('Error parsing JSON:', parseErr);
-        return null;
-      }
+async function getDate() {
+  try {
+    const data = await fs2.readFile(filePath, 'utf-8');
+    const jsonData = JSON.parse(data);
+    console.log('Time from file:', jsonData.date);
+    const oldTime = jsonData.date;
+    const diff = Math.abs((new Date(currentDate) - new Date(oldTime)));
+    if (diff >= 5000) {
+      // console.log('通过', diff);
+      await setDate();
+      return true;
+    } else {
+      // console.log('不通过', diff);
+      return false;
     }
-  });
+  } catch (err) {
+    // console.error('Error:', err);
+    return null;
+  }
 }
-// 设置时间当前
-function setDate() {
+
+async function setDate() {
   const contentToWrite = { date: currentDate };
-  fs.writeFile(filePath, JSON.stringify(contentToWrite, null, 2), (writeErr) => {
-    if (writeErr) {
-      console.error('Error creating file:', writeErr);
-    } else {
-      console.log('File created with current time:', currentDate);
-    }
-  });
+  await fs.promises.writeFile(filePath, JSON.stringify(contentToWrite, null, 2));
+  console.log('File created with current time:', currentDate);
+  return true;
 }
-exports.main = function() {
-  // 检查文件是否存在并读取或创建文件
-  fs.access(filePath, fs.constants.F_OK, (accessErr) => {
-    if (accessErr) {
-      // 文件不存在，创建新文件并写入当前时间
-      return setDate();
-    } else {
-      // 文件存在，读取文件内容并解析JSON
-      return getDate();
 
-    }
-  });
-}
+const throttle = async () => {
+  let result = null;
+  try {
+    await fs2.access(filePath, fs.constants.F_OK);
+    result = await getDate();
+  } catch (err) {
+    console.log('Error:', err);
+    result = await setDate();
+  }
+  return result;
+};
+
+// throttle().then(res => {
+//   console.log('throttle', res);
+// });
+
+module.exports = {
+  throttle,
+};
